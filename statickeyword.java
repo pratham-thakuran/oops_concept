8d1ec6eccf21809386e987481c37a4f5a839338b
pratham-thakuran <Pratham.11862@stu.upes.ac.in>
2026-05-09 13:31:51 +0530

    static keyword

--- statickeyword.java
+++ statickeyword.java
@@ -1,12 +1,10 @@
 
-class a{
-    static void greeting(){
-        System.out.println("hello world");
-    }
-}
-
 public class statickeyword {
     public static void main(String[] args){
         a.greeting();
+    }
+    class a{
+        static void greeting(){
+        System.out.println("hello world");
     }
 }
