115ce4027756d268da68c629c0a8c4d9cc354396
pratham-thakuran <Pratham.11862@stu.upes.ac.in>
2026-05-11 23:27:08 +0530

    swing1

---
+++ pehla.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class pehla {
+    public static void main(String[] args) {
+        JFrame a = new JFrame();
+        a.setTitle("my first frame");
+        a.setSize(400,300);
+        a.setVisible(true);
+        a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
